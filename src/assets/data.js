19ef942fccd48c0848209e1a47f6559e7f7046e6
santhoshshanigarapu <santhoshkanth777@gmail.com>
2025-01-05 17:01:52 +0530

    first commit

---
+++ src/assets/data.js
@@ -0,0 +1,42 @@
+export const data = [
+    {
+      question: "What does ReactJS help you build?",
+      option1: "Websites",
+      option2: "Databases",
+      option3: "User Interfaces",
+      option4: " Games",
+      ans: 3,
+    },
+    {
+      question: "Who developed ReactJS?",
+      option1: "Google",
+      option2: "Facebook",
+      option3: "Microsoft",
+      option4: "Twitter",
+      ans: 2,
+    },
+    {
+      question: "What command starts a new React application using Create React App?",
+      option1: "npm start",
+      option2: "npx create-react-app",
+      option3: "npm install",
+      option4: "node app.js",
+      ans: 2,
+    },
+    {
+      question: "Where do you write React code?",
+      option1: "Inside a database",
+      option2: "In the browser console",
+      option3: "Inside JavaScript files",
+      option4: "On a server",
+      ans: 3,
+    },
+    {
+      question: " What is the correct command to start a React development server?",
+      option1: "npm start",
+      option2: "npm run build",
+      option3: "npm install",
+      option4: "node start",
+      ans: 1,
+    },
+  ];
